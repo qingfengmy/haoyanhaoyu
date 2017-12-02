98b35eca73af7acfd3b3462f909ed1e246f46d69
michael <qingfengmy@126.com>
2017-12-02 16:30:41 +0800

    no message

---
+++ component/historyitem/historyitem.js
@@ -0,0 +1,30 @@
+// component/historyitem/historyitem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    history:{
+      type:Object,
+      value:{},
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    clickItem(){
+      wx.showToast({
+        title: '暂未实现',
+      })
+    }
+  }
+})
